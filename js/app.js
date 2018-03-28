c85e2100671e7c1398bea75f390d84f82cf801bd
Dor Bar <dor4231@gmail.com>
2018-03-28 23:43:05 +0300

    Pixel functionality, new background pattern added.
    A shadow for the canvas.

--- js/app.js
+++ js/app.js
@@ -1,9 +1,39 @@
 document.addEventListener("DOMContentLoaded", function () {
+    const reset_button = document.querySelector(".app-controls button");
+    const canvas = document.querySelector(".canvas");
+
+    let mouseDown = false;
+    let selectedColor = "#a00";
     const pageInit = function () {
 
     };
 
-    const reset_button = document.querySelector(".app-controls button");
+
+
+
+
+    ///////////////////
+    // Event Listeners:
+    ///////////////////
+
+    canvas.addEventListener("mousedown",function(event){
+        if (event.target.nodeName === "DIV") {
+            mouseDown = true;
+        }
+    }, true);
+
+    canvas.addEventListener("mouseover",function(event){
+        if (event.target.nodeName === "DIV" && mouseDown) {
+            event.target.style.backgroundColor = selectedColor;
+        }
+    }, true);
+
+    canvas.addEventListener("mouseup",function(event){
+        if (event.target.nodeName === "DIV") {
+            mouseDown = false;
+        }
+    }, true);
+
     reset_button.addEventListener("click", function (event) {
         event.preventDefault();
         const height = document.getElementById("height").value;
